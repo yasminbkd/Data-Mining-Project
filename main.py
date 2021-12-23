a503ad966c3c31263186bcf857ff0bc2ba959c82
yasminbkd <55556911+yasminbkd@users.noreply.github.com>
2021-12-23 12:26:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ import MachineLearning as ML
 
 
 def scrap():
-    path = r"C:\Users\MSI\Desktop\datamininggitsubmittests\chromedriver.exe"
-    df = SD.get_jobs('data scientist',1000, False, path, 15)
+    path = r"C:\Users\MSI\Desktop\datamininggitsubmittests\chromedriver.exe"#put the path in a variable to use it in all the projet
+    df = SD.get_jobs('data scientist',1000, False, path, 15)# we need to call the function we used in ScrapDataset 
     df.to_csv(r"dataset1.csv", encoding='utf-8', index= False)
     data_path = r"dataset1.csv"
     return data_path
 
 def clean(data_path):
-    df = CD.clean(data_path)
+    df = CD.clean(data_path)# we need to call the function we used in cleaningDataset using the scaped data
     df.to_csv(r"dataset_Cleaned.csv", encoding='utf-8')
     data_clean = r"dataset_Cleaned.csv"
     return data_clean
 
 def machinelearning(data_clean):
-    df = ML.machinelearning(data_clean)
+    df = ML.machinelearning(data_clean)# we need to call the function we used in machineLearning using the data cleaned
     df.to_csv(r"dataset_ML.csv", encoding='utf-8')
 
 
@@ -31,6 +31,7 @@ if __name__ == "__main__":
     parser.add_argument('--machinelearning', required=False, default="False")
     args = parser.parse_args()
     
+    #in the terminal when we want to do something about the data we need to write the title of function with True or False 
     if args.scrap=="True":
         data_path = scrap()
     if args.clean=="True":
